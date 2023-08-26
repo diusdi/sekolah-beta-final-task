6d264faeebd7b1579f2e005f2600b2c1213f1e90
diusdi <riadidion@gmail.com>
2023-08-26 16:55:56 +0700

    mengubah versi bootstrap ke versi 5

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,10 +10,16 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css' }
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
+      },
     ],
     script: [
-      { src: 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js', body:true}
+      {
+        src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js',
+        body: true,
+      },
     ],
   },
 
