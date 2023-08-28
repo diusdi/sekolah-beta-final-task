902d8fc0e9881c95f0bb80def34e5915674b0c5d
diusdi <riadidion@gmail.com>
2023-08-28 11:47:21 +0700

    memperbaiki tampilan footer

--- components/FooterWeb.vue
+++ components/FooterWeb.vue
@@ -1,10 +1,7 @@
 <template>
-  <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light p-5">
-      <span class="navbar-text">
-        &copy; 2023 UtangMU. Dionysius Yudha Riadi. Freshgraduate dari Blitar.
-        Ongoing Fullstack Developer
-      </span>
-    </nav>
+  <div class="footer">
+    <div class="container text-center">
+      <p>UtangMU &copy; 2023 Dionysius Yudha Riadi - Blitar - Freshgraduate</p>
+    </div>
   </div>
 </template>
