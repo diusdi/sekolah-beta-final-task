b2dc4949b14257d36708211197d0d8dc0c92698f
Dion <riadidion@gmail.com>
2023-09-16 15:43:26 +0700

    menghapus seluruh data

--- components/FooterWeb.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="footer">
-    <div class="container text-center">
-      <p>UtangMU &copy; 2023 Dionysius Yudha Riadi - Blitar - Freshgraduate</p>
-    </div>
-  </div>
-</template>
